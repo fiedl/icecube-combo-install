52c220dab57c205d753bb86adbea6cb930ceedd3
Sebastian Fiedlschuster <github@fiedlschuster.de>
2019-09-17 17:18:22 +0200

    github actions: call the build script from the github action.
    https://stackoverflow.com/a/31104898/2066546
    https://stackoverflow.com/a/52575123/2066546

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // https://stackoverflow.com/a/52575123/2066546
 
+const build_command = "bash -v -e ./install.sh"
+
 const execSync = require('child_process').execSync;
 // import { execSync } from 'child_process';  // replace ^ if using ES modules
-const output = execSync('ls', { encoding: 'utf-8' });  // the default is 'buffer'
-console.log('Output was:\n', output);
+
+execSync(build_command, { encoding: 'utf-8', stdio: 'inherit' });  // the default is 'buffer'
+
